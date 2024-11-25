2e41acf039fcf66de7eba027c813f8b21baead6a
Jonathan Santos <josantospy@gmail.com>
2024-11-25 15:15:33 -0400

    Iniciando proyecto

---
+++ build.sh
@@ -0,0 +1,9 @@
+source .venv/Scripts/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=https://josantosdev-web-production.up.railway.app reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
